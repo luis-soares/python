48ad8fc2b1bca3844d85462ec529f6382149e342
Luis Antonio Soares da Silva <luissoares@outlook.com>
2020-10-13 09:08:16 +0100

    function test

--- for.py
+++ for.py
@@ -1,5 +1,5 @@
 value = 0
-for num in range (101):
+for num in range (2, 10 , 2): # Start on 2 at 10 . 2 in 2 numbers.
     print ("Somando o numero", num)
     value = value + num
     print ("Por enquanto ja deu", value)
---
+++ functions.py
@@ -0,0 +1,34 @@
+import random
+import sys
+
+
+num = 0
+
+def test1():
+    global num
+    num += 1
+    
+
+def print_test1():
+    print ('only to test functions and global vars')
+    print ('this function was exec', num)
+
+
+def hello(name):
+    print('Hello', end=' ') #test function end from print
+    print (name)
+
+hello('Luis')
+hello('Fla')
+test1()
+print_test1()
+test1()
+print_test1()
+test1()
+print_test1()
+test1()
+print_test1()
+
+sys.exit()
+
+print ('test')
